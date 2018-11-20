3fe26b89bda8978f612c6b16cd93aa45e0118fb1
Sam <32162434+smbalf@users.noreply.github.com>
2018-11-20 22:15:40 +0000

    Update Game.py
    day one end

--- Game.py
+++ Game.py
@@ -1,34 +1,48 @@
+import sys
+import pickle
+import os
 import time
+os.system("clr")
 
-#defines the bid price calculation
+"""
+Guess we need to create some player files...
+Info to include:
+  -Player name
+  -Company name
+  -Current cash value
+  -Inventory held
+"""
+
+
+#Defines the bid price calculation
 def bid_price(price_change, base=100):
   return round(price_change * base, 3)
 
-#commodity pid prices
+#Commodity pid prices
 iron_bid = bid_price(1)
 tin_bid = bid_price(1.15)
 coal_bid = bid_price(0.9)
 
-#defines the offer price calculation
+#Defines the offer price calculation
 def offer_price(bid):
   return round(bid + (bid * 0.01), 3)
 
-#commodity offer prices
+#Commodity offer prices
 iron_offer = offer_price(iron_bid)
 tin_offer = offer_price(tin_bid)
 coal_offer = offer_price(coal_bid)
 
-#price list text input
+#Price list text input
 iron = "Iron Ore: " + "   " + str(iron_bid) + "    " + str(iron_offer)
 tin = "Tin Ore : " + "   " + str(tin_bid) + "  " + str(tin_offer)
 coal = "Coal Ore: " + "   " + str(coal_bid) + "   " + str(coal_offer)
 
-#commodity price list 
+#Commodity price list 
 price_list = [iron, 
               tin, 
               coal]
 
-#the commodity display
+#The commodity display
 def comm_display():
   print("")
   print("---------------------------------")
@@ -38,21 +52,27 @@ def comm_display():
   print("---------------------------------")
   print("")
 
-#GameStart
+#Game Start
 
 print("Welcome to Merchant Trader.")
 time.sleep(1)
 print("")
 input("Press any key to start the game...")
 print("")
-#need some kinda menu here
+
+"""
+Need the game start menu here
+  - Display player's cash and inventory
+  - Press P to see current prices
+"""
+
 while True:
-  print("Here is a list of today's prices:")
+  print("Here is a list of current prices:")
   time.sleep(2)
-  #shows the commodity price display
+  #Shows the commodity price display
   comm_display()
-  time.sleep(3)
-  #player input
+  time.sleep(2)
+  #Player input
   trade = input("Would you like to place a trade? (Y / N)")
   if trade.lower() == "n":
     print("")
@@ -62,10 +82,14 @@ while True:
   elif trade.lower() == "y":
       print("")
       break 
-#player decides which commodity to trade
+#Player decides which commodity to trade
 print("Which commodity would you like to trade?")
 print("")
-print("")
-#player decides bid or offer
-#player can't offer w/o having stock or purchase more than can afford
-#print("you have bought " + commodity bought + ".")
+
+"""
+Player chooses between Bid and Offer (B/O)
+  - Player inputs number they wish to B/O
+  - Can't O w/o inventory or can't B w/o cash
+print("You have sold " + str(comm_sold) + ".")
+or
+print("you have bought " + str(comm_bought) + ".")"""
